07d3347e719e41a7af45ff0476211223fa79ad67
Joe Alves <joseph.michael.alves@gmail.com>
2022-01-13 14:24:54 -0500

    Final product

--- app.js
+++ app.js
@@ -12,8 +12,41 @@ const getRandomDogImage = () => {
 
 const gallery = document.querySelector("#gallery");
 
-for (let i = 0; i < 100; i++) {
+const placeDogInHallOfFame = (imgSrc) => {
   const newImg = document.createElement("img");
-  newImg.src = "/pics/" + getRandomDogImage();
+  newImg.src = imgSrc;
   gallery.appendChild(newImg);
-}
+};
+
+const placeDogsInForVoting = () => {
+
+  // Access left-side and right-side imgs
+  const leftSideImg = document.querySelector("#left-side img");
+  const rightSideImg = document.querySelector("#right-side img");
+
+  // pick two random dogs
+  const randomDogs = [getRandomDogImage(), getRandomDogImage()];
+
+  // create new elements with those dogs as a src
+  leftSideImg.src = "/pics/" + randomDogs[0];
+  rightSideImg.src = "/pics/" + randomDogs[1];
+
+};
+
+placeDogsInForVoting();
+
+const votingInterface = document.querySelector("#voting-interface");
+
+votingInterface.addEventListener("click", (event) => {
+
+  const elementClickedOn = event.target;
+
+  if (elementClickedOn.tagName === "BUTTON") {
+    const imgOfDogNextToThisButton = elementClickedOn.previousElementSibling;
+    const imgSrc = imgOfDogNextToThisButton.src;
+    placeDogInHallOfFame(imgSrc);
+    placeDogsInForVoting();
+  }
+
+});
+
