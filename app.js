d58b5317a0e692d3460b93e3f8462a21d5222d7d
Joe Alves <joseph.michael.alves@gmail.com>
2022-01-13 14:11:18 -0500

    Visual stuff done, moving on to JS

--- app.js
+++ app.js
@@ -1,87 +1,19 @@
-const h1Element = document.querySelector("#message");
-const mainElement = document.querySelector("main");
-const sectionElement = document.querySelector("section");
+// console.log(window.APP_DOG_IMAGES);
 
-const createAnotherElement = (whatItShouldSay) => {
-  const newH2 = document.createElement("h2");
-  newH2.innerText = whatItShouldSay;
-  document.body.appendChild(newH2);
-  newH2.addEventListener('click', function () {
-    console.log("hi!");
-  });
+// const newImgElement = document.createElement("img");
+// newImgElement.src = "/pics/" + window.APP_DOG_IMAGES[1781];
+// document.body.appendChild(newImgElement);
+
+const getRandomDogImage = () => {
+  const howManyDogsIHave = window.APP_DOG_IMAGES.length;
+  const randomNumber = Math.floor(Math.random() * howManyDogsIHave);
+  return window.APP_DOG_IMAGES[randomNumber];
 };
 
-h1Element.addEventListener("click", function (eventDetails) {
-  eventDetails.stopPropagation();
-  if (eventDetails.x < 150) {
-    createAnotherElement("Hi Violet");
-  } else {
-    createAnotherElement("Hi Joe");
-  }
-});
+const gallery = document.querySelector("#gallery");
 
-h1Element.addEventListener("click", function (eventDetails) {
-  console.log("second listener for h1");
-});
-
-sectionElement.addEventListener("click", function (e) {
-  e.stopPropagation();
-  console.log("SECTION ELEMENT LOG");
-});
-
-mainElement.addEventListener("click", function (eventObject) {
-  eventObject.stopPropagation();
-  console.log("MAIN ELEMENT LOG");
-});
-
-// document.body.addEventListener("click", function (banana) {
-//   createAnotherElement("You missed!");
-// });
-
-window.addEventListener("keydown", (keyboardEvent) => {
-  createAnotherElement("you typed on the body!");
-  console.log("typed something!");
-});
-
-document.querySelector("#text-input").addEventListener("keydown", (e) => {
-  e.stopPropagation();
-});
-
-// h1Element.addEventListener("mousemove", function (mouseMoveEvent) {
-//   console.log(mouseMoveEvent.x, mouseMoveEvent.y);
-// });
-
-// h1Element.addEventListener("mouseenter", function () {
-//   console.log("i'm in here!");
-// });
-
-// h1Element.addEventListener("mouseleave", function () {
-//   console.log("I have left!");
-// });
-
-// Event delegation examples below ---
-
-const group = document.querySelector("#button-group");
-
-group.addEventListener("click", function (event) {
-
-  console.log(this);
-
-  const isFirst = event.target.innerText === "First";
-  const isSecond = event.target.innerText === "Second";
-  const isThird = event.target.innerText === "Third";
-
-  if (isFirst) {
-    console.log("First button clicked!");
-    const newButton = document.createElement("button");
-    newButton.innerText = "Whatever";
-    group.appendChild(newButton);
-  } else if (isSecond) {
-    console.log("Woohoo!");
-  } else if (isThird) {
-    console.log("Number three milord");
-  } else {
-    console.log(event.target.innerText);
-  }
-  
-});
+for (let i = 0; i < 100; i++) {
+  const newImg = document.createElement("img");
+  newImg.src = "/pics/" + getRandomDogImage();
+  gallery.appendChild(newImg);
+}
